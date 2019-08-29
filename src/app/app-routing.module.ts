5ecf871919b4e852b86d940803256a6e7c0b3462
halliwell2046 <halliwell2046@gmail.com>
2019-08-28 20:43:53 -0400

    profile and table comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddressComponent } from './dog-owner-create/address/address.component';
-
+import { DogProfileComponent } from './dog-owner-create/dog-profile/dog-profile.component';
+import { DogTableComponent } from './dog-owner-create/dog-table/dog-table.component';
 
 
 const routes: Routes = [
   { path: '', component: AddressComponent},
+  { path: 'alice', component: DogProfileComponent},
+  { path: 'alice2', component: DogTableComponent },
 ];
 
 @NgModule({
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,16 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatInputModule } from "@angular/material";
+import { MatRadioModule } from '@angular/material/radio';
+import {MatTableModule} from '@angular/material/table';
 
 const material = [
   MatInputModule,
   MatCheckboxModule,
   MatButtonModule,
-  BrowserAnimationsModule
+  BrowserAnimationsModule,
+  MatRadioModule,
+  MatTableModule
 ];
 @NgModule({
   declarations: [],
