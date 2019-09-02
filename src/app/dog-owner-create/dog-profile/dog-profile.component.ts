f14228a92a56116d75be8d1ebad7dac6fab6e137
halliwell2046 <halliwell2046@gmail.com>
2019-09-02 17:23:22 -0400

    dog profile

--- src/app/dog-owner-create/dog-profile/dog-profile.component.ts
+++ src/app/dog-owner-create/dog-profile/dog-profile.component.ts
@@ -3,9 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dog-profile',
   templateUrl: './dog-profile.component.html',
+  // template: `<button (click)="onClickMe()">Update!</button>
+  // {{clickMessage}}`,
+  // `input (keyup)="onKey"($event)">
+  // <p>{{values}}</p>`,
+
   styleUrls: ['./dog-profile.component.css']
 })
 export class DogProfileComponent implements OnInit {
+  // clickMessage = '';
+  // // values = '';
+
+  // onClickMe() {
+  //   this.clickMessage = 'Update!';
+
+  // onkeydown(event: any) {
+  //   this.values += event.target.value + ' | ';
+  // }
+  // }
 
   constructor() { }
 
