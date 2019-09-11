e6255de49afca3f544d06fba55cad9559469ca37
robert vanarsdall <robv@roberts-MacBook-Pro.local>
2019-09-11 19:38:41 -0400

    tracy final merge

--- src/app/containers/dashboard/dashboard.component.ts
+++ src/app/containers/dashboard/dashboard.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  accountType: string = sessionStorage.getItem('accountType')
+
   constructor() { }
 
   ngOnInit() {
