aa96f2838195397f2062c4c8159f250ca5d80811
robert vanarsdall <Rob@roberts-Mac.local>
2019-09-14 17:06:20 -0400

    minor fixes with model

--- src/app/owner-dashboard/request-walk/request-walk.component.ts
+++ src/app/owner-dashboard/request-walk/request-walk.component.ts
@@ -1,68 +1,72 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, OnInit, Inject } from "@angular/core";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from "@angular/material/dialog";
 
-export interface DialogData{
-  time: string
-date: string
+export interface DialogData {
+  time: string;
+  date: string;
 }
 @Component({
-  selector: 'app-request-walk',
-  templateUrl: './request-walk.component.html',
-  styleUrls: ['./request-walk.component.css']
+  selector: "app-request-walk",
+  templateUrl: "./request-walk.component.html",
+  styleUrls: ["./request-walk.component.css"]
 })
 export class RequestWalkComponent implements OnInit {
-  displayedColumns: string[] = ['date', 'time', 'dogs'];
+  displayedColumns: string[] = ["date", "time", "dogs"];
   requestWalk = [
     {
-      // dateRequested: '09/25/2019', 
+      // dateRequested: '09/25/2019',
       // timeRequested: '2:30PM',
       // walkerId: '1',
       // userId: '2',
       // isAccepted: true,
       // isCompleted: false,
       // ownerNotified: false,
-      // reviewTitle: 'Great', 
+      // reviewTitle: 'Great',
       // review: 'test',
       // rating: '****',
     }
-  ]
+  ];
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  time: string;
+  date: string;
+
+  addRequest(): void {
+    const dialogRef = this.dialog.open(DialogRequest, {
+      width: "250px",
+      data: { time: this.time, date: this.date }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+      this.requestWalk = [
+        {
+          dateRequested: result.date,
+          timeRequested: result.time
+        }
+      ];
+    });
   }
-
-time: string
-date: string
-
-addRequest():void{
-  const dialogRef = this.dialog.open(DialogRequest, {
-    width: '250px',
-    data: {time: this.time, date: this.date}
-  })
-
-  dialogRef.afterClosed().subscribe(result =>{
-    console.log(result)
-  })
-
-
-}
-
 }
 
 @Component({
-  selector: 'dialog-request',
-  templateUrl: 'dialog-request.html',
+  selector: "dialog-request",
+  templateUrl: "dialog-request.html"
 })
-
 export class DialogRequest {
-
   constructor(
     public dialogRef: MatDialogRef<DialogRequest>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
   }
-
 }
