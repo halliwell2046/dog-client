ddf6baa5d05b4883a1d960416c21424c44d3d5c0
robert vanarsdall <robv@roberts-MacBook-Pro.local>
2019-09-15 12:42:58 -0400

    owner sidebar shows data from db now

--- src/app/walker-sidebar/walker-sidebar.component.ts
+++ src/app/walker-sidebar/walker-sidebar.component.ts
@@ -1,30 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { DoggoService } from "src/app/doggo.service";
 
 @Component({
-  selector: 'app-walker-sidebar',
-  templateUrl: './walker-sidebar.component.html',
-  styleUrls: ['./walker-sidebar.component.css']
+  selector: "app-walker-sidebar",
+  templateUrl: "./walker-sidebar.component.html",
+  styleUrls: ["./walker-sidebar.component.css"]
 })
 export class WalkerSidebarComponent implements OnInit {
-
-  walkerData:object = [
-    {
-      firstName: "Jill",
-      lastName: "Farmington",
-      rating: 4,
-      bio:
+  walkerData: object = {
+    firstName: "Jill",
+    lastName: "Farmington",
+    rating: 4,
+    bio:
       "I've been an animal lover since birth and I'm excited to meet your dog. We will definitely go on walks on the Monon Trail and play in Broad Ripple Park since I live across the street from it. I have tons of toys and treats also. Your dog will get plenty of attention from me and my dog. My dog loves having friends come along on walks with us."
-    }
-      ];
-      constructor(private doggoService: DoggoService) {}
-    
-      ngOnInit() {
-        // this.doggoService.walkerData().subscribe(( data ) => {
-        //   console.log(data);
-        //   this.walkerData=data;
-        // })
+  };
+  constructor(private doggoService: DoggoService) {}
+
+  ngOnInit() {
+    this.doggoService.getUserInfo().subscribe((requested: any) => {
+      this.walkerData = {
+        firstName: requested.data.firstName,
+        lastName: requested.data.lastName,
+        bio: requested.data.bio
+      };
+      if (requested.data.pic !== null || requested.data.pic !== " ") {
+        Object.assign(this.walkerData, { pic: requested.data.pic });
+      } else {
+        Object.assign(this.walkerData, { pic: "../../assets/walker-icon.png" });
       }
-    
-    }
-    
+    });
+  }
+}
