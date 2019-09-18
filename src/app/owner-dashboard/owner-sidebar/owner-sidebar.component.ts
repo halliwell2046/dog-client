eacb4416e94590bedb8ffb5ef966e4e1d845a103
robert vanarsdall <robv@roberts-MacBook-Pro.local>
2019-09-17 20:17:58 -0400

    fixed walker default profile

--- src/app/owner-dashboard/owner-sidebar/owner-sidebar.component.ts
+++ src/app/owner-dashboard/owner-sidebar/owner-sidebar.component.ts
@@ -9,14 +9,17 @@ import { DoggoService } from "src/app/doggo.service";
 export class OwnerSidebarComponent implements OnInit {
   firstName: string;
   lastName: string;
-  picture: string = "../../../assets/owner-icon.png";
+  picture: string = "../../assets/owner-icon.png";
   constructor(private doggoService: DoggoService) {}
 
   ngOnInit() {
     this.doggoService.getUserInfo().subscribe((requested: any) => {
       this.firstName = requested.data.firstName;
       this.lastName = requested.data.lastName;
-      if (requested.data.pic !== null || requested.data.pic !== " ") {
+      console.log(requested);
+      if (requested.data.pic == null || requested.data.pic == "") {
+        this.picture = "../../assets/owner-icon.png";
+      } else {
         this.picture = requested.data.pic;
       }
     });
