9c66fa35ca5abb674c4403883be8c69a3655401b
robert vanarsdall <robv@roberts-MacBook-Pro.local>
2019-09-21 19:16:04 -0400

    fix

--- src/app/owner-dashboard/recent-requests/recent-requests.component.ts
+++ src/app/owner-dashboard/recent-requests/recent-requests.component.ts
@@ -125,7 +125,7 @@ export class RecentRequestsComponent implements OnInit {
 
     return newRequestArray;
   }
-
+  $events: boolean;
   recieveMessages($events) {
     this.reviewToggle = $events;
   }
