1402f28665e9fff0b801cd645f825bdcefaa9f4e
robert vanarsdall <robv@roberts-MacBook-Pro.local>
2019-09-24 19:41:00 -0400

    show fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit {
     public doggoService: DoggoService
   ) {}
   ngOnInit() {}
-  login: any;
+  show: any;
   onSubmit() {
     this.doggoService
       .userLogin(this.service.loginForm.value)
